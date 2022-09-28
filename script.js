4bb8dac23f46610da1d27885a225561a50e276c0
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-28 13:14:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const board = document.querySelector('#board');
 const colors = ['#FF3366', '#9999FF', '#66CCCC', '#66FFCC', '#3399FF', '#FF9933'];
 const squaresNumber = 1504;
+
+for (let i = 0; i < squaresNumber; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseleave', () => removeColor(square));
+
+    board.append(square);
+}
